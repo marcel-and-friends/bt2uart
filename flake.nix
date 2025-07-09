0f4631328fa9748338e5d960e07265f23673aa6f
Vinicius Deolindo <git@vini.cat>
2025-07-09 13:34:36 -0300

    flake: update to esp-idf 5.4.2

--- flake.nix
+++ flake.nix
@@ -21,10 +21,12 @@
         overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
         pkgs = import nixpkgs { inherit system overlays; };
         esp32-toolchain = pkgs.esp-idf-esp32.override {
+          rev = "f5c3654a1c2d2a01f7f67def7a0dc48e691f63c0";
+          sha256 = "sha256-cLBUuQS1Y4iLZT/kb5GI/X7JZfSqQzQUDy5ODC+O9wU=";
+
           toolsToInclude = [
             "esp-clang"
             "xtensa-esp-elf"
-            "esp-rom-elfs"
           ];
         };
       in
